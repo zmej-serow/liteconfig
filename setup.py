57ea565bc95e57b38cab7524885650882b55de5c
Andrey Serov <42320563+zmej-serow@users.noreply.github.com>
2019-06-04 15:09:27 +0200

    Bump version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
 
 setuptools.setup(
     name="liteconfig",
-    version="0.0.3",
+    version="0.1.0",
     author="Zmej Serow",
     author_email="zmej.serow@gmail.com",
     description="Lightweight and configurable .ini config parser with dot notation property access.",
